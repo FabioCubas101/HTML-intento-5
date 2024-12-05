d01010b43779747f18420d5a6a7d8259b41c497b
Cubas <fabiocubasvalido@alumno.ieselrincon.es>
2024-12-05 17:04:09 +0000

    Tercera entrega de web

---
+++ Tarea_3_LMD/script.js
@@ -0,0 +1,4 @@
+// Selecciona el botón por su ID y agrega un evento click
+    // Muestra la fecha y hora actuales en el elemento con ID "demo"
+    document.getElementById("demo").innerHTML = Date();
+;
